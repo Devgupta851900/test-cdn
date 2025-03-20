4c863e51b88abcefe899ffcc3ddbb3cc5375fdbf
DevGupta <devgupta851900@gmail.com>
2025-03-20 10:41:02 +0530

    [chore]

---
+++ public/consumer/svg/index.js
@@ -0,0 +1,8 @@
+import stripPhoneIcon from "./strip-phone.svg";
+import fullStar from "./full-star.svg";
+import halfStar from "./half-star.svg";
+import emptyStar from "./empty-star.svg";
+import playstore from "./playstore.svg";
+import appstore from "./appstore.svg";
+
+export { stripPhoneIcon, fullStar, halfStar, emptyStar, playstore, appstore };
